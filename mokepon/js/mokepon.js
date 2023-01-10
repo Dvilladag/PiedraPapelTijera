a59ccee975fc0715e7beed95dd18808632287c0b
Daniela Villada <danivillada2214@gmail.com>
2023-01-10 16:21:54 -0500

    vidas y mensaje de resultado

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -8,106 +8,135 @@ const inputLordvill = document.getElementById("lordvill");
 const inputHunter = document.getElementById("hunter");
 const spanPetPlayer = document.getElementById("pet-player");
 const spanPetEnemy = document.getElementById("pet-enemy");
+let spanLivesPlayer = document.getElementById("lives-player");
+let spanLivesEnemy = document.getElementById("lives-enemy");
+
 let enemy = 0;
 let selectedPetPlayer = "";
 let selectedPetEnemy = "";
 let selectedAttackPlayer = "";
 let selectedAttackEnemy = "";
 let attackResult = "";
+let livesPlayer = 3;
+let livesEnemy = 3;
 
 //functions
 function randomNumber(min, max) {
-	return Math.floor(Math.random() * (max - min + 1) + min);
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 function selectPetEnemy() {
-	enemy = randomNumber(1, 3);
-	if (enemy === 1) {
-		selectedPetEnemy = "Jubub 🤖";
-	} else if (enemy === 2) {
-		selectedPetEnemy = "Lordvill 👑";
-	} else {
-		selectedPetEnemy = "Hunter 🐕‍🦺";
-	}
-	alert("Your enemy chose " + selectedPetEnemy);
-	spanPetEnemy.innerHTML = selectedPetEnemy;
+  enemy = randomNumber(1, 3);
+  if (enemy === 1) {
+    selectedPetEnemy = "Jubub 🤖";
+  } else if (enemy === 2) {
+    selectedPetEnemy = "Lordvill 👑";
+  } else {
+    selectedPetEnemy = "Hunter 🐕‍🦺";
+  }
+  alert("Your enemy chose " + selectedPetEnemy);
+  spanPetEnemy.innerHTML = selectedPetEnemy;
 }
 function selectPetPlayer() {
-	if (inputJubub.checked) {
-		selectedPetPlayer = "Jubub 🤖";
-	} else if (inputLordvill.checked) {
-		selectedPetPlayer = "Lordvill 👑";
-	} else {
-		selectedPetPlayer = "Hunter 🐕‍🦺";
-	}
+  if (inputJubub.checked) {
+    selectedPetPlayer = "Jubub 🤖";
+  } else if (inputLordvill.checked) {
+    selectedPetPlayer = "Lordvill 👑";
+  } else {
+    selectedPetPlayer = "Hunter 🐕‍🦺";
+  }
 
-	if (selectedPetPlayer === "") {
-		alert("You don't selected anything, please select a Pet.");
-	} else {
-		alert("You selected " + selectedPetPlayer);
-		spanPetPlayer.innerHTML = selectedPetPlayer;
-		selectPetEnemy();
-	}
+  if (selectedPetPlayer === "") {
+    alert("You don't selected anything, please select a Pet.");
+  } else {
+    alert("You selected " + selectedPetPlayer);
+    spanPetPlayer.innerHTML = selectedPetPlayer;
+    selectPetEnemy();
+  }
 }
 function selectAttackEnemy() {
-	const number = randomNumber(1, 3);
-	if (number === 1) {
-		selectedAttackEnemy = "Fire 🔥";
-	} else if (number === 2) {
-		selectedAttackEnemy = "Watter 💧";
-	} else {
-		selectedAttackEnemy = "Ground 🌿";
-	}
+  const number = randomNumber(1, 3);
+  if (number === 1) {
+    selectedAttackEnemy = "Fire 🔥";
+  } else if (number === 2) {
+    selectedAttackEnemy = "Watter 💧";
+  } else {
+    selectedAttackEnemy = "Ground 🌿";
+  }
 }
 function createMessage() {
-	clash()
-	const newMessage = document.createElement("p");
-	const newContent = document.createTextNode(
-		`Your pet attacked with ${selectedAttackPlayer}, the pet of the enemy attacked with ${selectedAttackEnemy}, ${attackResult}`
-	)
-	newMessage.appendChild(newContent);
-	const currentP = document.getElementById("messages");
-	currentP.appendChild(newMessage);
+  clash();
+  const newMessage = document.createElement("p");
+  const newContent = document.createTextNode(
+    `Your pet attacked with ${selectedAttackPlayer}, the pet of the enemy attacked with ${selectedAttackEnemy}, ${attackResult}`
+  );
+  newMessage.appendChild(newContent);
+  const currentP = document.getElementById("messages");
+  currentP.appendChild(newMessage);
+  checkLives();
+}
+function createFinalMessage(result) {
+  const newMessage = document.createElement("p");
+  const newContent = document.createTextNode(result);
+  newMessage.appendChild(newContent);
+  const currentP = document.getElementById("messages");
+  currentP.appendChild(newMessage);
 }
 function setAttack(attack) {
-	alert("You selected " + attack);
-	selectedAttackPlayer = attack;
+  alert("You selected " + attack);
+  selectedAttackPlayer = attack;
 }
 function clickFireButton() {
-	setAttack("Fire🔥");
-	selectAttackEnemy();
-	createMessage();
+  setAttack("Fire 🔥");
+  selectAttackEnemy();
+  createMessage();
 }
 function clickWatterButton() {
-	setAttack("Watter 💧");
-	selectAttackEnemy();
-	createMessage();
+  setAttack("Watter 💧");
+  selectAttackEnemy();
+  createMessage();
 }
 function clickGroundButton() {
-	setAttack("Ground 🌿");
-	selectAttackEnemy();
-	createMessage();
+  setAttack("Ground 🌿");
+  selectAttackEnemy();
+  createMessage();
+}
+function checkLives() {
+  if (livesEnemy === 0) {
+    createFinalMessage("YOU WIN");
+  } else if (livesPlayer === 0) {
+    createFinalMessage("YOU LOST");
+  }
 }
 function clash() {
-	if (selectedAttackPlayer === selectedAttackEnemy) {
-		attackResult = "It's a Tie";
-	} else if (
-		selectedAttackPlayer === "Fire 🔥" &&
-		selectedAttackEnemy === "Watter 💧"
-	) {
-		attackResult = "You lose";
-	} else if (
-		selectedAttackPlayer === "Watter 💧" &&
-		selectedAttackEnemy === "Ground 🌿"
-	) {
-		attackResult = "You lose";
-	} else if (
-		selectedAttackPlayer === "Ground 🌿" &&
-		selectedAttackEnemy === "Fire 🔥"
-	) {
-		attackResult = "You lose";
-	} else {
-		attackResult = "You win";
-	}
+  if (selectedAttackPlayer === selectedAttackEnemy) {
+    attackResult = "It's a Tie";
+  } else if (
+    selectedAttackPlayer === "Fire 🔥" &&
+    selectedAttackEnemy === "Watter 💧"
+  ) {
+    attackResult = "You lose";
+    livesPlayer--;
+    spanLivesPlayer.innerHTML = livesPlayer;
+  } else if (
+    selectedAttackPlayer === "Watter 💧" &&
+    selectedAttackEnemy === "Ground 🌿"
+  ) {
+    attackResult = "You lose";
+    livesPlayer--;
+    spanLivesPlayer.innerHTML = livesPlayer;
+  } else if (
+    selectedAttackPlayer === "Ground 🌿" &&
+    selectedAttackEnemy === "Fire 🔥"
+  ) {
+    attackResult = "You lose";
+    livesPlayer--;
+    spanLivesPlayer.innerHTML = livesPlayer;
+  } else {
+    attackResult = "You win";
+    livesEnemy--;
+    spanLivesEnemy.innerHTML = livesEnemy;
+  }
+  
 }
 
 //ejecuciones
---
+++ mokepon/js/spanLivesenemy.js
@@ -0,0 +1,3 @@
+function spanLivesenemy(
+	//ejecuciones
+);
